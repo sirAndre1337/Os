a970c89aa720b29618f4c75d4623a17d51f4958b
André Luis <andrelluis17@hotmail.com>
2021-11-26 19:08:23 -0300

    Finalizando as classes de dominio

---
+++ src/main/java/com/andre/os/domain/OS.java
@@ -0,0 +1,138 @@
+package com.andre.os.domain;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.andre.os.domain.enums.Prioridade;
+import com.andre.os.domain.enums.Status;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+public class OS {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+	private LocalDateTime dataAbertura;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+	private LocalDateTime dataFechamento;
+	private Integer prioridade;
+	private Integer status;
+	private String observacoes;
+	
+	@ManyToOne
+	@JoinColumn(name = "tecnico_id")
+	private Tecnico tecnico;
+	
+	@ManyToOne
+	@JoinColumn(name = "cliente_id")
+	private Cliente cliente;
+
+	public OS() {
+		this.setDataFechamento(LocalDateTime.now());
+		this.setPrioridade(Prioridade.BAIXA);
+		this.setStatus(Status.ABERTO);
+	}
+
+	public OS(Long id, Prioridade prioridade, Status status, String observacoes, Tecnico tecnico, Cliente cliente) {
+		this.id = id;
+		this.setDataFechamento(LocalDateTime.now());
+		this.prioridade = (prioridade == null) ? 0 : prioridade.getCod();
+		this.status = (status == null) ? 0 : status.getCod();
+		this.observacoes = observacoes;
+		this.tecnico = tecnico;
+		this.cliente = cliente;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public LocalDateTime getDataAbertura() {
+		return dataAbertura;
+	}
+
+	public void setDataAbertura(LocalDateTime dataAbertura) {
+		this.dataAbertura = dataAbertura;
+	}
+
+	public LocalDateTime getDataFechamento() {
+		return dataFechamento;
+	}
+
+	public void setDataFechamento(LocalDateTime dataFechamento) {
+		this.dataFechamento = dataFechamento;
+	}
+
+	public Prioridade getPrioridade() {
+		return Prioridade.toEnum(this.prioridade);
+	}
+
+	public void setPrioridade(Prioridade prioridade) {
+		this.prioridade = prioridade.getCod();
+	}
+
+	public Status getStatus() {
+		return Status.toEnum(this.status);
+	}
+
+	public void setStatus(Status status) {
+		this.status = status.getCod();
+	}
+
+	public String getObservacoes() {
+		return observacoes;
+	}
+
+	public void setObservacoes(String observacoes) {
+		this.observacoes = observacoes;
+	}
+
+	public Tecnico getTecnico() {
+		return tecnico;
+	}
+
+	public void setTecnico(Tecnico tecnico) {
+		this.tecnico = tecnico;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OS other = (OS) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
--- src/main/java/com/andre/os/domain/Tecnico.java
+++ src/main/java/com/andre/os/domain/Tecnico.java
@@ -1,6 +1,18 @@
 package com.andre.os.domain;
 
-public class Tecnico extends Pessoa {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Tecnico extends Pessoa implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@OneToMany(mappedBy = "tecnico")
+	private List<OS> list = new ArrayList<OS>();
 
 	public Tecnico() {
 		super();
@@ -8,6 +20,14 @@ public class Tecnico extends Pessoa {
 
 	public Tecnico(Long id, String nome, String cpf, String telefone) {
 		super(id, nome, cpf, telefone);
+	}
+
+	public List<OS> getList() {
+		return list;
+	}
+
+	public void setList(List<OS> list) {
+		this.list = list;
 	}
 
 }
